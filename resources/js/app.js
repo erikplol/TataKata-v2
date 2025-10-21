e8ec2f72829890ccfa4efa7060af3660f7516ae0
citylighxts <hanazizahn@gmail.com>
2025-10-22 01:32:29 +0700

    fix: delete dependency tailwindcss/vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,28 +1,5 @@
 import './bootstrap';
-import { checkHybrid, predictAI, uploadPDF, getReference } from './api';
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 Alpine.start();
-
-// For FastAPI endpoint debug
-
-// async function runHybridCheck() {
-//   const result = await checkHybrid("Saya pergi ke sekolah");
-//   console.log("checkHybrid result:", result);
-// }
-
-// async function runAIPrediction() {
-//   const aiResult = await predictAI("Halo dunia");
-//   console.log("predictAI result:", aiResult);
-// }
-
-// async function runMaskPrediction() {
-//   const maskResult = await predictMask("Saya [MASK] ke sekolah");
-//   console.log("predictMask result:", maskResult);
-// }
-
-// async function runGetReference() {
-//   const ref = await getReference("penulisan-kapital");
-//   console.log("getReference result:", ref);
-// }
